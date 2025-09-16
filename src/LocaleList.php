baf875636b76e2a0ecb93fecf637699bb1a83f3e
C. Fahner <cfahner.git.ixuk9phi@slendium.org>
2025-09-16 19:36:00 +0200

    Adds LocaleList

---
+++ src/LocaleList.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Slendium\Localization;
+
+use ArrayAccess, Countable, IteratorAggregate;
+
+/**
+ * An ordered priority list of locales.
+ * @since 1.0
+ * @extends ArrayAccess<int<0,max>,Locale>
+ * @extends IteratorAggregate<Locale>
+ * @author C. Fahner
+ * @copyright Slendium 2025
+ */
+interface LocaleList extends ArrayAccess, Countable, IteratorAggregate { }
